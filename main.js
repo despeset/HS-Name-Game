4960cc28f463395ba4a5061f0775b95d70aa0c63
Daniel Mendel <daniel@danielespeset.com>
2013-02-14 06:18:15 -0500

    Heavily refactored, documented & prettied

--- main.js
+++ main.js
@@ -1,39 +1,111 @@
-// assumes jQuery
-!function( scope, $ ){
+/**
+ * Hacker-School name game
+ * author: Daniel Espeset <daniel@danielespeset.com>
+ * released under the Never Graduate license
+ */
 
-    var HS = scope.HS = {}
-      , classy = function( className ){ return 'HS-namegame-' + className }
-        // fabricate our container ( overlay the screen entirely )
-      , $container = $('<div class="'+classy('container')+'"></div>').css({
-            background: 'rgba(0,0,0,0.8)'
+// assumes jQuery
+!!!function( scope, $ ){
+
+    // for the facilitators
+    $("a.batch-emails").remove()
+
+    /**
+     * Utilties
+     */
+
+    // returns a namespaced CSS classname
+    function classy ( className ) { return 'HS-namegame-' + className }
+    // returns a new <div> string
+    function div  ( className, innerHTML ) { return '<div class="'+classy(className)+'">'+(innerHTML||'')+'</div>' }
+    // returns a jQuery extended <div> string with style matching `className` applied
+    function $div ( className, innerHTML ) { return $( div.apply( this, arguments ) ).css(styles[className]||{}) }
+    // fisherYates array randomization, see: http://www.codinghorror.com/blog/2007/12/the-danger-of-naivete.html
+    function shuffle ( arr ) {
+      var i = arr.length, j, tempi, tempj
+      if ( i === 0 ) return false
+      while ( --i ) {
+         j = Math.floor( Math.random() * ( i + 1 ) )
+         tempi = arr[i]
+         tempj = arr[j]
+         arr[i] = tempj
+         arr[j] = tempi
+       }
+      return arr // should return something, but redundant -- array is mutated directly.
+    }
+
+    /**
+     * CSS
+     */
+
+    var styles = {
+        container: {
+            background: 'rgba(0,0,0,1)' // opaque to deter cheating :)
           , position:   'absolute'
           , top:        0
           , left:       0
           , width:      $(document).width()
           , height:     $(document).height()
-        })
-        // fabricate our start page ( welcome, intro + choose batches )
-      , $mainPage = $( '<div class="'+classy('mainPage')+'"><h1 style="padding-bottom:20px;">Learn Your Hackers!</h1></div>').css({
+        }
+        , mainPage: {
             width: '600px'
           , background: '#fff'
-          , 'border-radius': '5px'
           , margin: '20px auto'
           , padding: '10px 50px 50px'
-        }).appendTo( $container )
-        // checkboxes for selecting which batches to pull from for game
-      , $startContent = $('<div></div>').appendTo( $mainPage ) 
-      , $batchCheckboxes = $('<ul class="'+classy('batchCheckboxes')+'"></ul>').css({
+          , 'border-radius': '5px'
+        }
+        , batchCheckboxes: {
             margin: '10px 0'
-        }).appendTo( $startContent )
-      , $start = $('<input type="submit" value="start game" />').appendTo( $startContent )
-      , $board = $('<div class="'+classy('board')+'"></div>').css({ display: 'table', 'font-size': '14px' })
-      , $pics  = $('<div class="'+classy('pics')+'"></div>').appendTo($board)
-      , $names = $('<div class="'+classy('names')+'"></div>').appendTo($board).css('clear','both')
-      , $next  = $('<input type="submit" value="next" />').css('display','block').css('position','relative').css('bottom','0')
+        }
+        , board: { 
+            display: 'table'
+          , 'font-size': '13px' // support long names like `Nicholas Bergson-Shilcock`
+        }
+        , pics: {
+            margin: '20px 0 0'
+        }
+        , name: {
+            border: '1px solid #aaa'
+          , background: '#eee'
+          , cursor: 'pointer'
+          , 'text-align': 'center'
+          , 'font-size': '14px'
+          , padding: '3px 0'
+        }
+        , nextButton: {
+            display: 'block'
+          , position: 'relative'
+          , bottom: 0
+        }
+        , correct:   { 'float': 'left', background: '#00e837' } // hacker school green.
+        , incorrect: { 'float': 'left', background: '#ed4044' }
+        , wrongName: { 'text-decoration': 'line-through' }
+    }
 
     /**
-     * build our dataset, like: { "batch name": { "First Last": { pic: "imgSRC", right: 0, wrong: 0 } } }
+     * HTML elems
      */
+
+    // container for the whole shabang ( overlays page )
+    var $container = $div('container')
+        // start page ( welcome, intro + choose batches )
+      , $mainPage  = $div('mainPage','<h1>Learn Your Hackers!</h1>').appendTo( $container )
+        // checkboxes for selecting which batches to pull from for game
+      , $startContent = $div('startContent').appendTo( $mainPage ) 
+      , $batchCheckboxes = $('<ul class="'+classy('batchCheckboxes')+'"></ul>').css(styles.batchCheckboxes).appendTo( $startContent )
+        // game board & containers
+      , $board = $div('board')
+      , $pics  = $div('pics').appendTo($board)
+      , $names = $div('names').appendTo($board).css('clear','both')
+        // buttons
+      , $start = $('<input type="submit" value="start game" />').appendTo( $startContent )
+      , $next  = $('<input type="submit" value="next" />').css(styles.nextButton)
+
+    /**
+     * Init dataset from DOM, like: { "batch name": { "First Last": { pic: "imgSRC", right: 0, wrong: 0 } } }
+     */
+
+    var HS = {}
 
     $('#batches').children().each(function(){
         // initialize dataset for this batch
@@ -54,94 +126,119 @@
 
     // check the first box by default
     $batchCheckboxes.find('input').first().click()
-    // append our container and fade it in
+    // append container and fade it in
     $container.hide().appendTo($('body')).fadeTo( 250, 1 )
+    // start the game when ready
+    $start.click( setupGameState ).click( nameGame )
+    // abort the game on escape
+    $( document ).on('keydown', function abort( e ){
+        if( e.which === 27 ) // esc
+            $container.fadeTo( 250, 0, function(){ $(this).detach(); $(document).off('keydown', abort) })
+    })
+    // scroll to the top of the page if not already there
+    $('html,body').animate({
+        scrollTop: 0
+    }, 250 )
 
-    $start.click(function(e){
-        var enabled = {}
-        $batchCheckboxes.find('input:checked').each(function(){
-            enabled[$(this).val()] = true
-        })
-        for( batch in HS ){
-            if( !enabled.hasOwnProperty(batch) ) delete HS[batch]
-        }
+    function setupGameState(){
         $startContent.hide()
         $(this).detach()
         $('.'+classy('scorecard')).hide()
 
         $mainPage.append( $board )
-        nameGame()
-    })
-
-    // fisherYates
-    function shuffle ( arr ) {
-      var i = arr.length, j, tempi, tempj;
-      if ( i == 0 ) return false;
-      while ( --i ) {
-         j = Math.floor( Math.random() * ( i + 1 ) );
-         tempi = arr[i];
-         tempj = arr[j];
-         arr[i] = tempj;
-         arr[j] = tempi;
-       }
     }
     
     function nameGame(){
-        // compile our batches into a big group
+            // all the hackers for this round
         var gameData = {}
-        // store a list of names for this round
-        var round = []
+            // queue of names to be shown
+          , round = []
+            // map of enabled batches
+          , enabled = {}
+            // stores the matches made by the player
+          , roundMatches = []
+            // transports the match being built
+          , match = { pic: null, name: null }
+            // stores the player matches during each set of 4
+          , matches = []
+
+        // get enabled batches
+        $batchCheckboxes.find('input:checked').each(function(){
+            enabled[$(this).val()] = true
+        })
+
+        // populate gameData
         for( batch in HS ){
-            if( HS.hasOwnProperty(batch) )
+            if( enabled.hasOwnProperty(batch) ){
                 for( person in HS[batch] ){
                     if( HS[batch].hasOwnProperty(person) ){
                         gameData[person] = HS[batch][person]
                         round.push(person)
                     }
                 }
+            }
         }
-        // randomize the order of our round
-        shuffle( round )
 
-        renderBoard();
-        var roundMatches = []
+        // randomize! see: http://scoundrelswiki.com/ScoundrelsPatter
+        shuffle( round )
+        // render the gameboard
+        renderBoard()
 
         function renderBoard(){
-            var pics = [], names = [], len = round.length > 4 ? 4 : round.length;
+            // board data, pics for this set
+            var pics = []
+                // names for this set
+              , names = []
+                // set length ( max 4 )
+              , len = Math.min( 4, round.length )
 
-            if( len < 1 ){
-                scoreRound();
-                return false;
-            }
+            !function setup(){
+                // clear our board data
+                match = { pic: null, name: null }
+                matches = []
 
-            for( var i = 0; i<len; i++ ){
-                var person = round[i];
-                pics.push( '<img src="'+gameData[person].pic+'" />' )
-                names.push( '<div class="'+classy('person')+'">'+person+'</div>' )
-            }
-            shuffle( pics )
-            shuffle( names )
-            for( var i = 0; i<len; i++ ){
-                $pics.append( pics[i] )
-                $names.append( names[i] )
-            }
-            var match = { pic: null, name: null }
-            var matches = []
-            
+                // if there are no more people to identify, score up
+                if( len < 1 ) return scoreRound()
+
+                // populate the board data ( pics & names )
+                for( var i = 0; i<len; i++ ){
+                    var person = round[i]
+                    pics.push( '<img src="'+gameData[person].pic+'" />' )
+                    names.push( $div( 'name', person ) )
+                }
+
+                // Hi diddle diddle, the Queen is in the middle. When the money goes down, the lady can’t be found!
+                shuffle( pics )
+                // Follow it with your eye as I shuffle. Here it is, and now here, now here, and now—where?
+                shuffle( names )
+
+                // show the pictures & names
+                for( var i = 0; i<len; i++ ){
+                    $pics.append( pics[i] )
+                    $names.append( names[i] )
+                }
+
+            }()
+
+            /**
+             * Event Handlers
+             */
+
+            // when pics are clicked, fade out others
             $pics.find('img').click(function( e ){
                 match.pic = $(this).attr('src')
                 match.$pic = $(this)
-                if( match.name === null ) $pics.find('img').not( $(this) ).fadeTo( 250, 0.2 )
+                if( match.name === null ) $pics.find('img').css('opacity',1).not( $(this) ).fadeTo( 250, 0.2 )
                 scoreMatch()
             })
-
-            $names.find('div.'+classy('person')).click(function(e){
+            // when names are clicked, fade out others
+            $names.find('div.'+classy('name')).click(function(e){
                 match.name = $(this).html()
                 match.$name = $(this)
-                if( match.pic === null ) $names.find('div.'+classy('person')).not( $(this) ).fadeTo( 250, 0.2 )
+                if( match.pic === null ) $names.find('div.'+classy('name')).css('opacity',1).not( $(this) ).fadeTo( 250, 0.2 )
                 scoreMatch()
             })
-
+            // next board
             $next.unbind('click').click(function(){
                 $pics.children().detach()
                 $names.children().detach()
@@ -149,53 +246,82 @@
                 renderBoard()
             })
 
-            scoreMatch = function(){
-                if( match.pic && match.name ){
-                    match.$pic.detach()
-                    match.$name.detach()
-                    matches.push( match )
-                    match = { pic: null, name: null }
-                    $pics.find('img').add( $names.find('div') ).css('opacity',1)
-                    if( $pics.find('img').length === 1 ){
-                        match = { pic:   $pics.find('img').attr('src')
-                                , $pic:  $pics.find('img')
-                                , name:  $names.find('div.'+classy('person')).html() 
-                                , $name: $names.find('div.'+classy('person')) }
-                        scoreMatch();
-                    }
-                    if( $pics.find('img').length === 0 ){
-                        for( var i=0,u=matches.length;i<u;i++ ){
-                            if( gameData[matches[i].name].pic === matches[i].pic ){
-                                gameData[matches[i].name].right++
-                                matches[i].win = true
-                                $pics.append( $('<div class="'+classy('right')+'" style="float:left;"></div>').append( matches[i].$pic ).append( matches[i].$name.prepend('✔ ') ) )
-                            } else {
-                                gameData[matches[i].name].wrong++
-                                matches[i].win = false
-                                var $wrongName;
-                                for( var i2=0;i2<u;i2++ ){
-                                    if( matches[i].pic === gameData[matches[i2].name].pic )
-                                        $wrongName = matches[i2].$name.clone()
-                                }
-                                $pics.append( $('<div class="'+classy('wrong')+'" style="float:left;"></div>').append( '<img src="'+gameData[matches[i].name].pic+'" />' )
-                                                                                                              .append( $wrongName.prepend('✖ ').css('text-decoration','line-through') )
-                                                                                                              .append( matches[i].$name ) )
-                            }
-                            roundMatches.push( matches[i] )
-                        }
-                        while( i-- ){ round.shift() }
-                        $board.append( $next )
-                    }
-                }
-            }
+        }
 
-            scoreRound = function(){
-                var points = 0;
-                for( var i=0,u=roundMatches.length;i<u;i++ ){
-                    if( roundMatches[i].win ) points++
-                }
-                $board.append( $('<div class="'+classy('scorecard')+'"><h2>You got '+points+' right and '+(roundMatches.length-points)+' wrong.</h2></div>') ).append( $start )
+        /**
+         * Scoring and Processing
+         */
+
+        // when player matches a picture / name pair
+        function scoreMatch(){
+            // only run when match is finished
+            if( match.pic === null || match.name === null ) return false
+            // remove matched elements
+            match.$pic.detach()
+            match.$name.detach()
+            // move this match to our completed set
+            matches.push( match )
+            // reset match transport
+            match = { pic: null, name: null }
+            // restore full opacity
+            $pics.find('img').add( $names.find('div') ).css('opacity',1)
+            // if there's only 1 pic / name set remaining on the board, match it automatically.
+            if( $pics.find('img').length === 1 ){
+                match = { pic:   $pics.find('img').attr('src')
+                        , $pic:  $pics.find('img')
+                        , name:  $names.find('div.'+classy('name')).html() 
+                        , $name: $names.find('div.'+classy('name')) }
+                return scoreMatch( match )
             }
+            // if there are no remaining matches on the board, show the board results
+            return $pics.find('img').length ? true : scoreBoard()
+        }
+
+        // when player has matched all 4 sets
+        function scoreBoard(){
+            for( var i=0,u=matches.length;i<u;i++ ){
+                if( gameData[matches[i].name].pic === matches[i].pic ){
+                    // YES! Player knows this hacker's name.
+                    // +1 right answer for this hacker ( not yet used, but should be weighting the next round )
+                    gameData[matches[i].name].right++
+                    // mark as correct
+                    matches[i].win = true
+                    // show result
+                    $pics.append( $div('correct').append( matches[i].$pic ).append( matches[i].$name.prepend('✔ ') ) )
+                } else {
+                    // Hmm... player should probably pair with this hacker ASAP.
+                    // +1 wrong answer for this hacker ( not yet used, but should be weighting the next round )
+                    gameData[matches[i].name].wrong++
+                    // mark as incorrect
+                    matches[i].win = false
+                    // copy the name they got wrong to show with a strike-through
+                    var $wrongName
+                    for( var i2=0;i2<u;i2++ ){
+                        if( matches[i].pic === gameData[matches[i2].name].pic )
+                            $wrongName = matches[i2].$name.clone()
+                    }
+                    // show result
+                    $pics.append( $div('incorrect').append( '<img src="'+gameData[matches[i].name].pic+'" />' )
+                                                                                                  .append( $wrongName.prepend('✖ ').css(styles.wrongName) )
+                                                                                                  .append( matches[i].$name ) )
+                }
+                // remember this match for the full round score later
+                roundMatches.push( matches[i] )
+            }
+            // advance the round
+            while( i-- ){ round.shift() }
+            $board.append( $next )
+        }
+
+        // when player has matched all hackers in round
+        function scoreRound(){
+            // total correct answers
+            var points = 0
+            for( var i=0,u=roundMatches.length;i<u;i++ ){
+                if( roundMatches[i].win ) points++
+            }
+            // render scorecard & restart button
+            $board.append( $div('scorecard','<h2>You got '+points+' right and '+(roundMatches.length-points)+' wrong.</h2>') ).append( $start )
         }
 
     }
